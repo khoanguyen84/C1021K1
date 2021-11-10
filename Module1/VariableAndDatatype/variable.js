147dedbe111bade0ceea3ae46b417f838bbcd27a
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-11-10 15:06:05 +0700

    js variable and datatype

---
+++ Module1/VariableAndDatatype/variable.js
@@ -0,0 +1,19 @@
+let number1 = 5;
+let number2 = 7;
+// 5 + 7 = 12
+let total = number1 + number2;
+// console.log(number1 + ' + ' + number2 + ' = ' + total);
+
+// console.log(`${number1} + ${number2} = ${number1 + number2}`)
+
+let x = 5;
+let y = 5;
+// console.log(x == y);
+// console.log(x === y);
+// console.log((5 + 7) + '4' + (5 + 6))
+// console.log(++x + 5);
+// console.log(y++ + 5);
+// console.log(x)
+
+console.log(1 + '1'); //'11'
+console.log(1 * '11'); //NaN ! Not a Number | 1 | 0
